ace56c84f64325f37926dedcb2c8e65d40b00a47
krani4ka <nesytykha@gmail.com>
2021-09-18 19:14:37 +0700

    Работает загрузка img

--- shift_test/my_task/views.py
+++ shift_test/my_task/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .forms import UploadFileForm
+
+
+# Imaginary function to handle an uploaded file.
+# from somewhere import handle_uploaded_file
 
 
 def index(request):
-    return render(request, 'my_task/index.html')
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            # handle_uploaded_file(request.FILES['file'])
+            with open('name.png', 'wb+') as destination:
+                for chunk in request.FILES['file'].chunks():
+                    destination.write(chunk)
+        return render(request, 'my_task/index.html', {'foo': 'bar'})
+    else:
+        form = UploadFileForm()
+    return render(request, 'my_task/index.html', {'foo': 'baz'})
